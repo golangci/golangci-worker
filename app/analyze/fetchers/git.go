77439682a8744d66fd0c10e233a4e428875ff10c
Denis Isaev <denis@golangci.com>
2018-06-11 11:01:04 +0300

    add -q option to git clone

--- app/analyze/fetchers/git.go
+++ app/analyze/fetchers/git.go
@@ -10,7 +10,7 @@ import (
 type Git struct{}
 
 func (gf Git) Fetch(ctx context.Context, url, ref, destDir string, exec executors.Executor) error {
-	args := []string{"clone", "--depth", "1", "--branch", ref, url, destDir}
+	args := []string{"clone", "-q", "--depth", "1", "--branch", ref, url, destDir}
 	if out, err := exec.Run(ctx, "git", args...); err != nil {
 		return fmt.Errorf("can't run git cmd %v: %s, out is %s", args, err, out)
 	}
